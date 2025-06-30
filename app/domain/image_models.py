df8d543539f9ce8f7bfdce11bfb5a551bddfad5b
Wangnov <48670012+Wangnov@users.noreply.github.com>
2025-06-30 17:52:10 +0800

    删除ruff导致的格式化换行

--- app/domain/image_models.py
+++ app/domain/image_models.py
@@ -9,16 +9,12 @@ class ImageMetadata:
         self.size = size
         self.url = url
         self.delete_url = delete_url
-
-
 class UploadResponse:
     def __init__(self, success: bool, code: str, message: str, data: ImageMetadata):
         self.success = success
         self.code = code
         self.message = message
         self.data = data
-
-
 class ImageUploader:
     def upload(self, file: bytes, filename: str) -> UploadResponse:
         raise NotImplementedError
